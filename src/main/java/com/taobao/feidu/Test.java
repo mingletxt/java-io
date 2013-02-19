0b8141b0854dcf43e8a0c152ffd15bc6dcc22c3d
mingletxt <zhuomingemail@gmail.com>
2013-02-19 21:49:45 +0800

    test

--- src/main/java/com/taobao/feidu/Test.java
+++ src/main/java/com/taobao/feidu/Test.java
@@ -3,11 +3,12 @@ package com.taobao.feidu;
 /**
  * User: feidu
  * Date: 13-2-19
- * Time: обнГ2:18
+ * Time: О©╫О©╫О©╫О©╫2:18
  * To change this template use File | Settings | File Templates.
  */
 public class Test {
     public static void main(String[] args) {
         System.out.println("feidu");
+        System.out.println("feidu2");
     }
 }
